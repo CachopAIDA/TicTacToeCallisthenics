90c837642898f3e6560f9ccaaf7f4a308b1e44da
Claudio López Ardura <cllopez@aidacanarias.com>
2021-11-22 11:41:45 +0100

    Added first movement and second doesn't pass

--- TicTacToe-Calisthenics/TicTacToeSpecs.cs
+++ TicTacToe-Calisthenics/TicTacToeSpecs.cs
@@ -16,15 +16,62 @@ namespace TicTacToe_Calisthenics
             var startingBoard = new Game().Render();
             Assert.Equal(expectedBoard, startingBoard);
         }
+
+        [Fact]
+        public void first_movement_is_in_the_first_cell()
+        {
+            var expectedBoard = new string[]
+            {
+                String.Empty, String.Empty, String.Empty,
+                String.Empty, "X", String.Empty,
+                String.Empty, String.Empty, String.Empty,
+            };
+
+            Game game = new Game();
+            game.Play();
+            var resultBoard = game.Render();
+
+            Assert.Equal(expectedBoard, resultBoard);
+        }
+
+        [Fact]
+        public void second_movement_is_in_the_second_cell()
+        {
+            var expectedBoard = new string[]
+            {
+                String.Empty, String.Empty, String.Empty,
+                String.Empty, "X", "O",
+                String.Empty, String.Empty, String.Empty,
+            };
+
+            Game game = new Game();
+            game.Play();
+            game.Play();
+            var resultBoard = game.Render();
+
+            Assert.Equal(expectedBoard, resultBoard);
+        }
     }
 
     public class Game
     {
+        private string[] board = new string[] {
+            String.Empty, String.Empty, String.Empty,
+            String.Empty, String.Empty, String.Empty,
+            String.Empty, String.Empty, String.Empty,
+        };
+
         public IEnumerable<string> Render()
         {
-            return new string[] {
-                String.Empty, String.Empty, String.Empty, 
-                String.Empty, String.Empty, String.Empty, 
+            return board;
+        }
+
+        public void Play()
+        {
+            board = new string[]
+            {
+                String.Empty, String.Empty, String.Empty,
+                String.Empty, "X", String.Empty,
                 String.Empty, String.Empty, String.Empty,
             };
         }
