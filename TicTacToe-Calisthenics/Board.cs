e848ab778c0953b9d5f626aa51dfce5f7258bd98
Oscar Herrero <oherrero@aidacanarias.com>
2021-11-22 16:56:50 +0100

    Refactor cells and board

--- TicTacToe-Calisthenics/Board.cs
+++ TicTacToe-Calisthenics/Board.cs
@@ -1,5 +1,7 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualBasic;
 
 namespace TicTacToe_Calisthenics
 {
@@ -7,79 +9,95 @@ namespace TicTacToe_Calisthenics
     {
         private class Cell
         {
-            public virtual bool CheckPlayer ( string player )
+            public enum Line
             {
-                return false;
+                HorizontalTop,
+                HorizontalCenter,
+                HorizontalBottom,
+                VerticalLeft,
+                VerticalCenter,
+                VerticalRight,
+                DiagonalLeft,
+                DiagonalRight
+            }
+
+            private List<Line> lines;
+
+            private string mark;
+
+            public Cell(params Line[] lines)
+            {
+                this.mark = string.Empty;
+                this.lines = lines.ToList();
+            }
+
+            public void Mark(string mark)
+            {
+                if (!String.IsNullOrEmpty(this.mark)) throw new InvalidOperationException();
+                this.mark = mark;
+            }
+
+            public bool CheckPlayer ( string player )
+            {
+                return mark == player;
             }
 
             public override String ToString ()
             {
-                return String.Empty;
+                return mark;
+            }
+
+            public bool IsInLine(Line line)
+            {
+                return this.lines.Contains(line);
             }
         }
 
+        private readonly List<Cell> board;
 
-        private class CellX : Cell
+        public Board()
         {
-            public override bool CheckPlayer ( string player )
+            board = new List<Cell>()
             {
-                return player == "X";
-            }
-
-            public override String ToString ()
-            {
-                return "X";
-            }
+                new Cell(Cell.Line.HorizontalTop, Cell.Line.VerticalLeft, Cell.Line.DiagonalLeft),
+                new Cell(Cell.Line.HorizontalTop, Cell.Line.VerticalCenter),
+                new Cell(Cell.Line.HorizontalTop, Cell.Line.VerticalRight, Cell.Line.DiagonalRight),
+                new Cell(Cell.Line.HorizontalCenter, Cell.Line.VerticalLeft),
+                new Cell(Cell.Line.HorizontalCenter, Cell.Line.VerticalCenter, Cell.Line.DiagonalLeft, Cell.Line.DiagonalRight),
+                new Cell(Cell.Line.HorizontalCenter, Cell.Line.VerticalRight),
+                new Cell(Cell.Line.HorizontalBottom, Cell.Line.VerticalLeft, Cell.Line.DiagonalRight),
+                new Cell(Cell.Line.HorizontalBottom, Cell.Line.VerticalCenter),
+                new Cell(Cell.Line.HorizontalBottom, Cell.Line.VerticalRight, Cell.Line.DiagonalLeft)
+            };
         }
-
-
-        private class CellO : Cell
-        {
-            public override Boolean CheckPlayer ( String player )
-            {
-                return player == "O";
-            }
-
-            public override String ToString ()
-            {
-                return "O";
-            }
-        }
-
-        private string[] board = new[] {
-            String.Empty, String.Empty, String.Empty,
-            String.Empty, String.Empty, String.Empty,
-            String.Empty, String.Empty, String.Empty,
-        };
 
         public IEnumerable<string> Render()
         {
-            return board;
+            return board.Select(cell => cell.ToString());
         }
 
         public void Mark(short position, string player)
         {
-            if (!String.IsNullOrEmpty(this.board[position])) throw new InvalidOperationException();
-            this.board[position] = player;
+            this.board[position].Mark(player);
         }
 
         public bool CheckTicTacToe(string player)
         {
-            return CheckHorizontals()  ||
-                   CheckDiagonals() ;
+            return CheckHorizontals(player)  ||
+                   CheckDiagonals(player) ;
         }
 
-        private bool CheckHorizontals()
+        private bool CheckHorizontals(string player)
         {
-            return !string.IsNullOrEmpty(board[0]) && board[0] == board[1] && board[0] == board[2] ||
-                   !string.IsNullOrEmpty(board[3]) && board[3] == board[4] && board[4] == board[5] ||
-                   !string.IsNullOrEmpty(board[6]) && board[6] == board[7] && board[7] == board[8];
+            return board.Count(cell => cell.CheckPlayer(player) && cell.IsInLine(Cell.Line.HorizontalTop)) == 3 ||
+                   board.Count(cell => cell.CheckPlayer(player) && cell.IsInLine(Cell.Line.HorizontalCenter)) == 3 ||
+                   board.Count(cell => cell.CheckPlayer(player) && cell.IsInLine(Cell.Line.HorizontalBottom)) == 3;
         }
 
-        private bool CheckDiagonals()
+        private bool CheckDiagonals(string player)
         {
-            return !string.IsNullOrEmpty(board[4]) && board[4] == board[2] && board[2] == board[6] ||
-                   !string.IsNullOrEmpty(board[0]) && board[0] == board[4] && board[4] == board[8];
+            return board.Count(cell => cell.CheckPlayer(player) && cell.IsInLine(Cell.Line.DiagonalLeft)) == 3 ||
+                   board.Count(cell => cell.CheckPlayer(player) && cell.IsInLine(Cell.Line.DiagonalRight)) == 3;
         }
     }
 }
